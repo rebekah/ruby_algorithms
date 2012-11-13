22f98c0433f1fc9450199c85a9cfbf221918444f
Rebekah <rebekah@azimuthinternet.com>
2012-11-12 16:50:54 -0800

    working :)

--- quick_sort.rb
+++ quick_sort.rb
@@ -3,52 +3,50 @@ module QuickSort
   require 'active_support/core_ext/hash'
   
   def quicksort(array,from = 0,to = (array.length - 1))
-  puts "array: #{array}"
-  puts "from: #{from}"
-  puts "to: #{to}"
     length = to - from
-    puts "length: #{length}"
-    if length <= 1
-      array
-    else
+    if array.length <= 1
+      return array
+    else 
       divider = from
       if length % 2 == 0
         pivot_index = to - (length/2)
       else
         pivot_index = to - ((length + 1)/2)
       end
-      puts "pivot_index: #{pivot_index}"
       for index in (from..to)
-        puts "index_in_for_loop: #{index}"
         if array[index] < array[pivot_index] 
-          unless divider == index 
-            swap(array,index,divider)
+          unless divider == index
+            if divider == pivot_index
+              pivot_index = index
+            end
+            array = swap(array,index,divider)
           end
           divider += 1
         end
       end
-      if pivot_index < divider
-        index_to_left_of_divider = divider - 1
-        swap(array, pivot_index,index_to_left_of_divider)
-        pivot_index = index_to_left_of_divider
-      else
-        swap(array, pivot_index,divider)
-        pivot_index = divider
+      array = swap(array, pivot_index,divider)
+      pivot_index = divider
+      unless [from,(from+1)].include?(divider)
+        less_then_pivot_array_start = from
+        less_then_pivot_array_end = pivot_index - 1
+        quicksort(array, less_then_pivot_array_start, less_then_pivot_array_end)
       end
-      less_then_pivot_array_start = 0
-      less_then_pivot_array_end = pivot_index - 1
-      more_then_pivot_array_start = pivot_index + 1
-      more_then_pivot_array_end = array.length - 1
-      quicksort(array, less_then_pivot_array_start, less_then_pivot_array_end)
-      quicksort(array, more_then_pivot_array_start, more_then_pivot_array_end)
+      unless [to,(to + 1)].include?(divider)
+        more_then_pivot_array_start = pivot_index + 1
+        more_then_pivot_array_end = to
+        quicksort(array, more_then_pivot_array_start, more_then_pivot_array_end)
+      end
     end
+    array
   end
   
+
   def swap(array,item_1_index,item_2_index)
       swap_item_1 = array[item_1_index]
       swap_item_2 = array[item_2_index]
       array[item_1_index] = swap_item_2
       array[item_2_index] = swap_item_1
+      array
   end
     
 end
--- test_quick_sort.rb
+++ test_quick_sort.rb
@@ -3,11 +3,23 @@ require 'test/unit'
 
 
 class TestQuickSort < Test::Unit::TestCase
+
   def setup
     @instance = Class.new{ include QuickSort }.new
   end
 
   def test_quicksort
-    assert_equal([1,2,2,4,5,7,8,9], @instance.quicksort([4,5,2,1,7,8,9,2])) # test the output value given the input params
+    array = []
+    for number in 1..100
+      array.push(number)
+    end
+    shuffled_array = array.shuffle
+    assert_equal(array, @instance.quicksort(shuffled_array))
+    assert_equal([1,2,2,4,5,7,8,9], @instance.quicksort([8,5,2,4,2,7,9,1]))
   end
+  
+  def test_swap
+    assert_equal([2,1,3,4], @instance.swap([1,2,3,4],0,1))
+  end
+  
 end
